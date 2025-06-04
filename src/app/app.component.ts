c92c2373093797e870226008454f05b9d7012290
albertoconec <alberto.martinez.301202@gmail.com>
2025-06-04 18:20:17 +0200

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductosListaComponent } from './components/productos-lista/productos-lista.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ProductosListaComponent],
+  imports: [RouterOutlet,ProductosListaComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
